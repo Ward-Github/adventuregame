dfc27c413142457baa8e489711c95a9859939f6e
Ward <113605493+Ward-Github@users.noreply.github.com>
2022-09-27 16:42:50 +0200

    Begint ergens op te lijken

---
+++ main.py
@@ -0,0 +1,18 @@
+inventory = []
+
+def begin():
+    print('''
+You’ve awakened traveller,
+welcome to mountain Karamja. 
+This mountain has been terrorized by the Demi-human called Saradomin,
+It is your task to free us from his wrath.
+On your left side there is a small house, Follow the path through the woods and you will find it.
+On your right side there is a cave, Follow the path over the bridge to the cave.
+    ''')
+    direction = input("What direction would you like to go (right/left): ")
+    if direction ==  'right':
+        print("You have chose to follow the path to the cave")
+    elif direction == 'left':
+        print("You have chosen the path through the woods to the small house.")
+    else:
+        print("invalid choice.")
--- upper_word.py
+++ upper_word.py
@@ -15,12 +15,4 @@ def upper_word_puzzle():
         else:
             print('The code failed. Maybe something different will work? ')
 
-def small_house():
-    print("""
-You enter a small broken down home. It's very dusty and you can barely see. In the corner u spot an old chest with a lock on it
-    """)
-    choice = input('Do you want to try to open it or leave the home (open/leave): ')
-    if choice == 'open':
-        upper_word_puzzle()
-    else:
-        print('Leaved huis')
+upper_word_puzzle()
