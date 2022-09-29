d53c1951f80a4dc7a3ae04fe5f94f1f8da7c3e96
Ward <113605493+Ward-Github@users.noreply.github.com>
2022-09-29 16:29:44 +0200

    weer gefixt

---
+++ delencode/random/died.py
@@ -0,0 +1,8 @@
+import os
+
+clear = lambda: os.system('cls')
+
+def died():
+    clear()
+    input('Thank you for playing our game! Sadly you have not made it to the end. Press enter to try again! ')
+    #begin()
--- lever_riddle.py
+++ lever_riddle.py
@@ -1,3 +1,10 @@
+import os
+
+clear = lambda: os.system('cls')
+
+def died():
+    clear()
+    input('Thank you for playing our game! Sadly you have not made it to the end. Press enter to try again! ')
 
 def lever_riddle():
     print(''' Welcome traveller. 
@@ -11,30 +18,30 @@ If you choose the incorrect lever you will be met with the fate of death.
 If you choose the correct lever you will be allowed to continue.
     ''')
 
-    fate = input("Do you wish to continue? Yes/No")
-    if fate == 'No':
+    fate = input("Do you wish to continue (yes/no): ")
+    if fate == 'no':
         print("There is no returning back once you started, You have died.")
-        #died()
-    elif fate == 'Yes':
+        died()
+    elif fate == 'yes':
         print("""Very well, To learn the name of the correct wall you must solve this clue.
     This answer to this clue is the name of the correct wall, and will guide you to the right direction.
     """)
     else:
         print("Should have answered with yes or no, fool. You have died.")
-        #died()
+        died()
 
     answer = input("What is made of water, but when placed in water it will die?")
     if answer == 'Snow':
         print("Congratulations traveller, you are smart enough to continue on ur path. Best of luck.")
     elif answer == 'Grass':
         print("Unfortunately you are clearly not cut out for this. Im sorry but we must diminish ur existence.")
-        #died()
+        died()
     elif answer == 'Sand':
         print("Unfortunately you are clearly not cut out for this. Im sorry but we must diminish ur existence.")
-        #died()
+        died()
     elif answer == 'Fog':
         print("Unfortunately you are clearly not cut out for this. Im sorry but we must diminish ur existence.")
-        #died()
+        died()
 
 lever_riddle()
 
